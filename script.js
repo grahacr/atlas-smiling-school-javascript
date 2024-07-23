0da8262f86fd0f13684a472accd7737b835a093e
grahacr <courtney.graham@atlasschool.com>
2024-07-22 21:52:04 -0500

    almost there

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ $(document).ready(function() {
           let activeQuote = index === 0 ? 'active' : '';
           let quoteHTML = `
           <div class="carousel-item ${activeQuote}">
-            <div class="row mx-auto align-items-center">
-              <div class="col-12 col-sm-2 col-lg-2 text-center">
+            <div class="row m-4 align-items-center justify-content-center">
+              <div class="col-12 col-sm-2 col-lg-2 text-center px-4">
                 <img src="${quote.pic_url}" class="d-block align-self-center" alt="Quote person">
                 </div>
-                <div class="col-12 col-sm-7 col-lg-9">
+                <div class="col-12 col-sm-7 col-lg-9 px-4">
                   <div class="quote-text">
                     <p class="text-white">${quote.text}</p>
                     <h4 class="text-white">${quote.name}</h4>
