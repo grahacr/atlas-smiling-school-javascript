a4115db5be0db1bdf2d95b06dddfa8b58d984ef5
grahacr <courtney.graham@atlasschool.com>
2024-07-24 14:53:18 -0500

    not working yet but working on 0-homepage to do most popular videos carousel

--- script.js
+++ script.js
@@ -1,35 +1,96 @@
-$(document).ready(function() {
-  function getQuotes() {
-    $('.loader').addClass('d-block');
-    $.ajax({
-      url: 'https://smileschool-api.hbtn.info/quotes',
-      method: 'GET',
-      success: function(data) {
-        $('.loader').removeClass('d-block');
-        $('.carousel-inner').empty();
+function getQuotes() {
+  $('.loader').addClass('d-block');
+  $.ajax({
+    url: 'https://smileschool-api.hbtn.info/quotes',
+    method: 'GET',
+    success: function(data) {
+      $('.loader').removeClass('d-block');
+      $('.carousel-inner').empty();
 
-        data.forEach(function(quote, index) {
-          let activeQuote = index === 0 ? 'active' : '';
-          let quoteHTML = `
-          <div class="carousel-item ${activeQuote}">
-            <div class="row m-4 align-items-center justify-content-center">
-              <div class="col-12 col-sm-2 col-lg-2 text-center px-4">
-                <img src="${quote.pic_url}" class="d-block align-self-center" alt="Quote person">
-                </div>
-                <div class="col-12 col-sm-7 col-lg-9 px-4">
-                  <div class="quote-text">
-                    <p class="text-white">${quote.text}</p>
-                    <h4 class="text-white">${quote.name}</h4>
-                    <span class="text-white">${quote.title}</span>
-                  </div>
+      data.forEach(function(quote, index) {
+        let activeQuote = index === 0 ? 'active' : '';
+        let quoteHTML = `
+        <div class="carousel-item ${activeQuote}">
+          <div class="row m-4 align-items-center justify-content-center">
+            <div class="col-12 col-sm-2 col-lg-2 text-center px-4">
+              <img src="${quote.pic_url}" class="d-block align-self-center" alt="Quote person">
+              </div>
+              <div class="col-12 col-sm-7 col-lg-9 px-4">
+                <div class="quote-text">
+                  <p class="text-white">${quote.text}</p>
+                  <h4 class="text-white">${quote.name}</h4>
+                  <span class="text-white">${quote.title}</span>
                 </div>
               </div>
-            </div>`;
-            $('.carousel-inner').append(quoteHTML);
+            </div>
+          </div>`;
+          $('.carousel-inner').append(quoteHTML);
         });
-        $('.carousel').carousel();
       }
     });
   }
+
+  function getPopularVideos() {
+    $('.video-loader').addClass('d-block');
+    $.ajax({
+      url: 'https://smileschool-api.hbtn.info/popular-tutorials',
+      method: 'GET',
+      success: function(data) {
+        $('.video-loader').removeClass('d-block');
+        $('.carousel-inner').empty();
+
+        data.forEach(function(video, index) {
+          let activeVideo = index === 0 ? 'active' : '';
+          let starHTML = '';
+          for (let i = 0; i < video.star; i++) {
+            starHTML += `<img src="images/star_on.png" alt="star rating" width="15px">`
+          }
+          let videoHTML = `
+          <div class="carousel-item ${activeVideo}">
+            <div class="row align-items-center mx-auto">
+              <div class="col-12 col-sm-6 col-md-6 col-lg-3 d-flex justify-content-center justify-content-md-end justify-content-lg-center">
+                <div class="card">
+                <img
+                  src="${video.thumb_url}"
+                  class="card-img-top"
+                  alt="Video thumbnail"/>
+                <div class="card-img-overlay text-center">
+                <img
+                  src="images/play.png"
+                  alt="Play"
+                  width="64px"
+                  class="align-self-center play-overlay"/>
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title font-weight-bold">${video.title}</h5>
+                  <p class="card-text text-muted">${video.sub-title}</p>
+                  <div class="creator d-flex align-items-center">
+                    <img
+                      src="${video.author_pic_url}"
+                      alt="Creator of Video"
+                      width="30px"
+                      class="rounded-circle"/>
+                    <h6 class="pl-3 m-0 main-color">${video.author}</h6>
+                  </div>
+                  <div class="info pt-3 d-flex justify-content-between">
+                    <div class="rating">${starHTML}
+                  </div>
+                  <span class="main-color">${video.duration}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`;
+      $('.carousel-inner').append(videoHTML);
+      });
+    },
+    error: function(error) {
+      console.error("Error fetching video data", error);
+    }
+  });
+}
+$(document).ready(function() {
   getQuotes();
+  getPopularVideos();
 });
